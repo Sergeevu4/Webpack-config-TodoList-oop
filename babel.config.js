434892fb4ea7fe22781289c0e3becb0aa3925fd8
Sergeevu4 <sergei.sergeevu4@gmail.com>
2019-05-18 00:44:29 +0300

    babel +plugins: Lodash + transform-runtime

--- babel.config.js
+++ babel.config.js
@@ -5,18 +5,18 @@ module.exports = (api) => {
       '@babel/preset-env',
       {
         useBuiltIns: 'usage',
-        debug: true,
-
-        corejs: '3',
+        debug: true, // команда необходимая для наглядности подключаемых плагинов
+        corejs: { version: 3, proposals: true },
         targets: ['> 1%', 'ie 11', 'not op_mini all'],
       },
     ],
   ];
 
-  // const plugin = [];
+  const plugins = ['@babel/plugin-transform-runtime', 'lodash'];
+  // lodash необходим для обработки путей при обращении к функция
 
   return {
     presets,
-    // plugin,
+    plugins,
   };
 };
